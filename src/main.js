26ed122b9781b2174fe07bfabc5da764f46cc0f2
AlanYoung <2306915539@qq.com>
2019-12-05 16:40:54 +0800

    header样式 webpack配置别名 iconfont stylus配置变量

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
 import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import '@/assets/styles/border.css'
+import '_styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="header">
-      <div class="header-left">返回</div>
-      <div class="header-input">输入城市/景点/游玩主题</div>
-      <div class="header-right">城市</div>
+      <div class="header-left">
+        <div class="iconfont back-icon">&#xe624;</div>
+      </div>
+      <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
+      </div>
+      <div class="header-right">
+        城市
+        <span class="iconfont arrow-icon">&#xe62d;</span>
+      </div>
     </div>
 </template>
 
@@ -13,14 +21,20 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  // import "../../../assets/style/variable.styl"
+  @import '~_styles/variable.styl';
+  // .24rem = 12px
   .header
     line-height : .86rem
     display : flex
-    background : #00bcd4
+    background : $bgColor
     color : #fff
     .header-left
       width: .64rem
       float : left
+      .back-icon
+         text-align : center
+         font-size : .4rem
     .header-input
       flex : 1
       line-height : .64rem
@@ -28,10 +42,14 @@ export default {
       border-radius: .1rem
       margin-top: .12rem
       margin-left: .2rem
+      padding-left : .2rem
       height: .64rem
       color: #ccc
     .header-right
       width : 1.24rem
       float : right
       text-align : center
+      .arrow-icon
+        margin-left : -.10rem
+        font-size : .24rem
 </style>
